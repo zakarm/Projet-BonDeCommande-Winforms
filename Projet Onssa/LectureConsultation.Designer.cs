cca6c757292aedb23b035b6dc6ba0f4201919587
Zakariae Mrabet <zakariaemrabet1@gmail.com>
2021-04-29 22:25:46 +0000

    valider

---
+++ Projet Onssa/LectureConsultation.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Projet_Onssa
+{
+    partial class LectureConsultation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.cb_Consultation = new System.Windows.Forms.ComboBox();
+            this.cb_Fournisseur = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(954, 575);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // cb_Consultation
+            // 
+            this.cb_Consultation.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cb_Consultation.FormattingEnabled = true;
+            this.cb_Consultation.Location = new System.Drawing.Point(475, 5);
+            this.cb_Consultation.Name = "cb_Consultation";
+            this.cb_Consultation.Size = new System.Drawing.Size(155, 21);
+            this.cb_Consultation.TabIndex = 1;
+            this.cb_Consultation.SelectedIndexChanged += new System.EventHandler(this.cb_Consultation_SelectedIndexChanged);
+            // 
+            // cb_Fournisseur
+            // 
+            this.cb_Fournisseur.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cb_Fournisseur.FormattingEnabled = true;
+            this.cb_Fournisseur.Location = new System.Drawing.Point(636, 5);
+            this.cb_Fournisseur.Name = "cb_Fournisseur";
+            this.cb_Fournisseur.Size = new System.Drawing.Size(155, 21);
+            this.cb_Fournisseur.TabIndex = 2;
+            this.cb_Fournisseur.SelectedIndexChanged += new System.EventHandler(this.cb_Fournisseur_SelectedIndexChanged);
+            // 
+            // LectureConsultation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 575);
+            this.Controls.Add(this.cb_Fournisseur);
+            this.Controls.Add(this.cb_Consultation);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "LectureConsultation";
+            this.Text = "Lecture Pvj";
+            this.Load += new System.EventHandler(this.LectureConsultation_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.ComboBox cb_Consultation;
+        private System.Windows.Forms.ComboBox cb_Fournisseur;
+    }
+}
---
+++ Projet Onssa/LectureConsultation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet_Onssa
+{
+    public partial class LectureConsultation : Form
+    {
+        public LectureConsultation()
+        {
+            InitializeComponent();
+        }
+
+        DataSetReport ds = new DataSetReport();
+        DataSetReportTableAdapters.ConsultationSetTableAdapter dac = new DataSetReportTableAdapters.ConsultationSetTableAdapter();
+        DataSetReportTableAdapters.ConsultationFournisseurTableAdapter dacf = new DataSetReportTableAdapters.ConsultationFournisseurTableAdapter();
+        
+        DataSetReportTableAdapters.FournisseurSetTableAdapter daf = new DataSetReportTableAdapters.FournisseurSetTableAdapter();
+        
+
+        private void cb_Consultation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (OnssaModelContainer4 ctx = new OnssaModelContainer4())
+            {
+
+
+                daf.Fill(ds.FournisseurSet);
+                var query = from f in ds.FournisseurSet
+                            join c in ds.ConsultationFournisseur
+                            on f.IdFournisseur equals c.ListFournisseur_IdFournisseur
+                            where c.ListConsultation_IdConsultation == (int)cb_Consultation.SelectedValue
+                            select new
+                            {
+                                Nom = f.Nom,
+                                Num = f.IdFournisseur,
+                            };
+                cb_Fournisseur.DisplayMember = "Nom";
+                cb_Fournisseur.ValueMember = "Num";
+                cb_Fournisseur.DataSource = query.ToList();
+                           
+            }
+        }
+
+         private void LectureConsultation_Load(object sender, EventArgs e)
+        {
+            dacf.Fill(ds.ConsultationFournisseur);
+            dac.Fill(ds.ConsultationSet);
+
+            using (OnssaModelContainer4 ctx = new OnssaModelContainer4())
+            {
+                cb_Consultation.DisplayMember = "Numconsultation";
+                cb_Consultation.ValueMember = "idconsultation";
+                cb_Consultation.DataSource= ctx.ConsultationSet.ToList();
+
+            }
+
+        }
+
+        private void cb_Fournisseur_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            int i = int.Parse(cb_Fournisseur.SelectedValue.ToString()); 
+            daf.FillByFournniseur(ds.FournisseurSet,i);
+
+            CrystalReportConsultation ce = new CrystalReportConsultation();
+
+            ce.SetDataSource(ds);
+            crystalReportViewer1.ReportSource = ce;
+            crystalReportViewer1.Refresh();
+        }
+    }
+}
