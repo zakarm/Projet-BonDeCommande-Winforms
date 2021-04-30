9f3460edb8384d9ac25948c6049211c2358a01d6
yassin mouja <yassin_momo_villa@hotmail.fr>
2021-04-30 16:26:47 +0000

    VALIDER

--- Projet Onssa/LectureOv.Designer.cs
+++ Projet Onssa/LectureOv.Designer.cs
@@ -30,29 +30,31 @@ namespace Projet_Onssa
         private void InitializeComponent()
         {
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.cb_Op = new System.Windows.Forms.ComboBox();
+            this.cb_Ov = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.CrystalReportOv1 = new Projet_Onssa.CrystalReportOv();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
             // 
-            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.ActiveViewIndex = 0;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.CrystalReportOv1;
             this.crystalReportViewer1.Size = new System.Drawing.Size(984, 614);
             this.crystalReportViewer1.TabIndex = 0;
             // 
-            // cb_Op
+            // cb_Ov
             // 
-            this.cb_Op.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.cb_Op.FormattingEnabled = true;
-            this.cb_Op.Location = new System.Drawing.Point(584, 6);
-            this.cb_Op.Name = "cb_Op";
-            this.cb_Op.Size = new System.Drawing.Size(155, 21);
-            this.cb_Op.TabIndex = 2;
+            this.cb_Ov.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cb_Ov.FormattingEnabled = true;
+            this.cb_Ov.Location = new System.Drawing.Point(584, 6);
+            this.cb_Ov.Name = "cb_Ov";
+            this.cb_Ov.Size = new System.Drawing.Size(155, 21);
+            this.cb_Ov.TabIndex = 2;
             // 
             // label1
             // 
@@ -61,9 +63,9 @@ namespace Projet_Onssa
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(251)))), ((int)(((byte)(251)))));
             this.label1.Location = new System.Drawing.Point(481, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Ordre de paiement";
+            this.label1.Text = "Ordre de virement";
             // 
             // LectureOv
             // 
@@ -71,10 +73,11 @@ namespace Projet_Onssa
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 614);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.cb_Op);
+            this.Controls.Add(this.cb_Ov);
             this.Controls.Add(this.crystalReportViewer1);
             this.Name = "LectureOv";
             this.Text = "LectureOv";
+            this.Load += new System.EventHandler(this.LectureOv_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,7 +86,8 @@ namespace Projet_Onssa
         #endregion
 
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
-        private System.Windows.Forms.ComboBox cb_Op;
+        private System.Windows.Forms.ComboBox cb_Ov;
         private System.Windows.Forms.Label label1;
+        private CrystalReportOv CrystalReportOv1;
     }
 }
--- Projet Onssa/LectureOv.cs
+++ Projet Onssa/LectureOv.cs
@@ -12,9 +12,50 @@ namespace Projet_Onssa
 {
     public partial class LectureOv : Form
     {
+        DataSetReport ds = new DataSetReport();
+        DataSetReportTableAdapters.ProduitSetTableAdapter dap = new DataSetReportTableAdapters.ProduitSetTableAdapter();
+        DataSetReportTableAdapters.ModeleDevisSetTableAdapter dam = new DataSetReportTableAdapters.ModeleDevisSetTableAdapter();
+        DataSetReportTableAdapters.ModeleDevisProduitTableAdapter damp = new DataSetReportTableAdapters.ModeleDevisProduitTableAdapter();
+        DataSetReportTableAdapters.OVSetTableAdapter daov = new DataSetReportTableAdapters.OVSetTableAdapter();
+        DataSetReportTableAdapters.OISetTableAdapter daoi = new DataSetReportTableAdapters.OISetTableAdapter();
+        DataSetReportTableAdapters.BCSetTableAdapter dabc = new DataSetReportTableAdapters.BCSetTableAdapter();
+        DataSetReportTableAdapters.OPSetTableAdapter daop = new DataSetReportTableAdapters.OPSetTableAdapter();
+        DataSetReportTableAdapters.FESetTableAdapter dafe = new DataSetReportTableAdapters.FESetTableAdapter();
+        DataSetReportTableAdapters.PVJSetTableAdapter dapvj = new DataSetReportTableAdapters.PVJSetTableAdapter();
+        DataSetReportTableAdapters.FournisseurSetTableAdapter daf = new DataSetReportTableAdapters.FournisseurSetTableAdapter();
+        DataSetReportTableAdapters.PVJFournisseurTableAdapter dapvjf = new DataSetReportTableAdapters.PVJFournisseurTableAdapter();
         public LectureOv()
         {
             InitializeComponent();
+        }
+
+        private void LectureOv_Load(object sender, EventArgs e)
+        {
+            dabc.Fill(ds.BCSet);
+            dafe.Fill(ds.FESet);
+            dapvj.Fill(ds.PVJSet);
+            daf.Fill(ds.FournisseurSet);
+            dapvjf.Fill(ds.PVJFournisseur) ;
+            dap.Fill(ds.ProduitSet);
+
+            daov.Fill(ds.OVSet);
+
+            daop.Fill(ds.OPSet);
+
+            daoi.Fill(ds.OISet);
+
+            damp.Fill(ds.ModeleDevisProduit);
+
+            dam.Fill(ds.ModeleDevisSet);
+
+            cb_Ov.DisplayMember = "NuMM";
+            cb_Ov.ValueMember = "IdOv";
+
+            CrystalReportOv ce = new CrystalReportOv();
+
+            ce.SetDataSource(ds);
+            crystalReportViewer1.ReportSource = ce;
+            crystalReportViewer1.Refresh();
         }
     }
 }
