8d55edfd67af2c72b2378f7e94efc7395f03f59e
Zakariae Mrabet <zakariaemrabet1@gmail.com>
2021-04-30 14:53:59 +0000

    valider

--- Projet Onssa/LectureConsultation.Designer.cs
+++ Projet Onssa/LectureConsultation.Designer.cs
@@ -32,6 +32,8 @@ namespace Projet_Onssa
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.cb_Consultation = new System.Windows.Forms.ComboBox();
             this.cb_Fournisseur = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
@@ -42,14 +44,14 @@ namespace Projet_Onssa
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(954, 575);
+            this.crystalReportViewer1.Size = new System.Drawing.Size(984, 614);
             this.crystalReportViewer1.TabIndex = 0;
             // 
             // cb_Consultation
             // 
             this.cb_Consultation.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cb_Consultation.FormattingEnabled = true;
-            this.cb_Consultation.Location = new System.Drawing.Point(475, 5);
+            this.cb_Consultation.Location = new System.Drawing.Point(474, 16);
             this.cb_Consultation.Name = "cb_Consultation";
             this.cb_Consultation.Size = new System.Drawing.Size(155, 21);
             this.cb_Consultation.TabIndex = 1;
@@ -59,17 +61,41 @@ namespace Projet_Onssa
             // 
             this.cb_Fournisseur.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cb_Fournisseur.FormattingEnabled = true;
-            this.cb_Fournisseur.Location = new System.Drawing.Point(636, 5);
+            this.cb_Fournisseur.Location = new System.Drawing.Point(635, 16);
             this.cb_Fournisseur.Name = "cb_Fournisseur";
             this.cb_Fournisseur.Size = new System.Drawing.Size(155, 21);
             this.cb_Fournisseur.TabIndex = 2;
             this.cb_Fournisseur.SelectedIndexChanged += new System.EventHandler(this.cb_Fournisseur_SelectedIndexChanged);
             // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(251)))), ((int)(((byte)(251)))));
+            this.label1.Location = new System.Drawing.Point(472, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Consultation";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(251)))), ((int)(((byte)(251)))));
+            this.label2.Location = new System.Drawing.Point(632, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fournniseur";
+            // 
             // LectureConsultation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(954, 575);
+            this.ClientSize = new System.Drawing.Size(984, 614);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cb_Fournisseur);
             this.Controls.Add(this.cb_Consultation);
             this.Controls.Add(this.crystalReportViewer1);
@@ -77,6 +103,7 @@ namespace Projet_Onssa
             this.Text = "Lecture Pvj";
             this.Load += new System.EventHandler(this.LectureConsultation_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +112,7 @@ namespace Projet_Onssa
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
         private System.Windows.Forms.ComboBox cb_Consultation;
         private System.Windows.Forms.ComboBox cb_Fournisseur;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
---
+++ Projet Onssa/LectureOv.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Projet_Onssa
+{
+    partial class LectureOv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.cb_Op = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(984, 614);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // cb_Op
+            // 
+            this.cb_Op.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cb_Op.FormattingEnabled = true;
+            this.cb_Op.Location = new System.Drawing.Point(584, 6);
+            this.cb_Op.Name = "cb_Op";
+            this.cb_Op.Size = new System.Drawing.Size(155, 21);
+            this.cb_Op.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(251)))), ((int)(((byte)(251)))));
+            this.label1.Location = new System.Drawing.Point(481, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ordre de paiement";
+            // 
+            // LectureOv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 614);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_Op);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "LectureOv";
+            this.Text = "LectureOv";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.ComboBox cb_Op;
+        private System.Windows.Forms.Label label1;
+    }
+}
