23c69ce89bd999dfc9d027cd197e272003207905
Zakariae Mrabet <zakariaemrabet1@gmail.com>
2021-05-26 18:30:53 +0100

    valider

--- Projet Onssa/ConsultationForm.Designer.cs
+++ Projet Onssa/ConsultationForm.Designer.cs
@@ -39,6 +39,9 @@ namespace Projet_Onssa
             this.btn_Supprimer = new Bunifu.Framework.UI.BunifuFlatButton();
             this.dgv_Fournisseur = new Bunifu.Framework.UI.BunifuCustomDataGrid();
             this.materialCard1 = new MaterialSkin.Controls.MaterialCard();
+            this.date_delai = new Bunifu.Framework.UI.BunifuDatepicker();
+            this.bunifuMetroTextbox3 = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.label4 = new System.Windows.Forms.Label();
             this.date_envoi = new Bunifu.Framework.UI.BunifuDatepicker();
             this.bunifuMetroTextbox2 = new Bunifu.Framework.UI.BunifuMetroTextbox();
             this.label3 = new System.Windows.Forms.Label();
@@ -50,9 +53,6 @@ namespace Projet_Onssa
             this.cb_Num = new System.Windows.Forms.ComboBox();
             this.bunifuMetroTextbox1 = new Bunifu.Framework.UI.BunifuMetroTextbox();
             this.label8 = new System.Windows.Forms.Label();
-            this.date_delai = new Bunifu.Framework.UI.BunifuDatepicker();
-            this.bunifuMetroTextbox3 = new Bunifu.Framework.UI.BunifuMetroTextbox();
-            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Fournisseur)).BeginInit();
             this.materialCard1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -63,7 +63,7 @@ namespace Projet_Onssa
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(35, 187);
+            this.label2.Location = new System.Drawing.Point(38, 229);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(114, 21);
             this.label2.TabIndex = 18;
@@ -225,7 +225,7 @@ namespace Projet_Onssa
             this.dgv_Fournisseur.RowHeadersWidth = 5;
             this.dgv_Fournisseur.RowTemplate.Height = 80;
             this.dgv_Fournisseur.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.dgv_Fournisseur.Size = new System.Drawing.Size(870, 271);
+            this.dgv_Fournisseur.Size = new System.Drawing.Size(870, 234);
             this.dgv_Fournisseur.TabIndex = 33;
             // 
             // materialCard1
@@ -259,6 +259,49 @@ namespace Projet_Onssa
             this.materialCard1.Padding = new System.Windows.Forms.Padding(14);
             this.materialCard1.Size = new System.Drawing.Size(955, 515);
             this.materialCard1.TabIndex = 156;
+            // 
+            // date_delai
+            // 
+            this.date_delai.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.date_delai.BackColor = System.Drawing.Color.Transparent;
+            this.date_delai.BorderRadius = 0;
+            this.date_delai.ForeColor = System.Drawing.Color.Black;
+            this.date_delai.Format = System.Windows.Forms.DateTimePickerFormat.Long;
+            this.date_delai.FormatCustom = null;
+            this.date_delai.Location = new System.Drawing.Point(42, 180);
+            this.date_delai.Name = "date_delai";
+            this.date_delai.Size = new System.Drawing.Size(364, 30);
+            this.date_delai.TabIndex = 154;
+            this.date_delai.Value = new System.DateTime(2021, 4, 30, 14, 1, 42, 86);
+            // 
+            // bunifuMetroTextbox3
+            // 
+            this.bunifuMetroTextbox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bunifuMetroTextbox3.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(109)))), ((int)(((byte)(239)))));
+            this.bunifuMetroTextbox3.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bunifuMetroTextbox3.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(109)))), ((int)(((byte)(239)))));
+            this.bunifuMetroTextbox3.BorderThickness = 1;
+            this.bunifuMetroTextbox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.bunifuMetroTextbox3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.bunifuMetroTextbox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bunifuMetroTextbox3.isPassword = false;
+            this.bunifuMetroTextbox3.Location = new System.Drawing.Point(36, 177);
+            this.bunifuMetroTextbox3.Margin = new System.Windows.Forms.Padding(4);
+            this.bunifuMetroTextbox3.Name = "bunifuMetroTextbox3";
+            this.bunifuMetroTextbox3.Size = new System.Drawing.Size(375, 35);
+            this.bunifuMetroTextbox3.TabIndex = 153;
+            this.bunifuMetroTextbox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(32, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 152;
+            this.label4.Text = "Date delai";
             // 
             // date_envoi
             // 
@@ -339,9 +382,9 @@ namespace Projet_Onssa
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.dgv_Fournisseur);
-            this.panel1.Location = new System.Drawing.Point(38, 216);
+            this.panel1.Location = new System.Drawing.Point(38, 253);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(878, 279);
+            this.panel1.Size = new System.Drawing.Size(878, 242);
             this.panel1.TabIndex = 146;
             // 
             // txtarea_Objet
@@ -355,7 +398,7 @@ namespace Projet_Onssa
             this.txtarea_Objet.Font = new System.Drawing.Font("Century Gothic", 9.75F);
             this.txtarea_Objet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.txtarea_Objet.isPassword = false;
-            this.txtarea_Objet.Location = new System.Drawing.Point(38, 107);
+            this.txtarea_Objet.Location = new System.Drawing.Point(35, 107);
             this.txtarea_Objet.Margin = new System.Windows.Forms.Padding(4);
             this.txtarea_Objet.Name = "txtarea_Objet";
             this.txtarea_Objet.Size = new System.Drawing.Size(375, 35);
@@ -367,7 +410,7 @@ namespace Projet_Onssa
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(34, 83);
+            this.label6.Location = new System.Drawing.Point(31, 83);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(186, 20);
             this.label6.TabIndex = 142;
@@ -414,49 +457,6 @@ namespace Projet_Onssa
             this.label8.Size = new System.Drawing.Size(203, 20);
             this.label8.TabIndex = 141;
             this.label8.Text = "Numéro de la Consultation";
-            // 
-            // date_delai
-            // 
-            this.date_delai.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.date_delai.BackColor = System.Drawing.Color.Transparent;
-            this.date_delai.BorderRadius = 0;
-            this.date_delai.ForeColor = System.Drawing.Color.Black;
-            this.date_delai.Format = System.Windows.Forms.DateTimePickerFormat.Long;
-            this.date_delai.FormatCustom = null;
-            this.date_delai.Location = new System.Drawing.Point(486, 180);
-            this.date_delai.Name = "date_delai";
-            this.date_delai.Size = new System.Drawing.Size(364, 30);
-            this.date_delai.TabIndex = 154;
-            this.date_delai.Value = new System.DateTime(2021, 4, 30, 14, 1, 42, 86);
-            // 
-            // bunifuMetroTextbox3
-            // 
-            this.bunifuMetroTextbox3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.bunifuMetroTextbox3.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(109)))), ((int)(((byte)(239)))));
-            this.bunifuMetroTextbox3.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuMetroTextbox3.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(109)))), ((int)(((byte)(239)))));
-            this.bunifuMetroTextbox3.BorderThickness = 1;
-            this.bunifuMetroTextbox3.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMetroTextbox3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.bunifuMetroTextbox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuMetroTextbox3.isPassword = false;
-            this.bunifuMetroTextbox3.Location = new System.Drawing.Point(480, 177);
-            this.bunifuMetroTextbox3.Margin = new System.Windows.Forms.Padding(4);
-            this.bunifuMetroTextbox3.Name = "bunifuMetroTextbox3";
-            this.bunifuMetroTextbox3.Size = new System.Drawing.Size(375, 35);
-            this.bunifuMetroTextbox3.TabIndex = 153;
-            this.bunifuMetroTextbox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            // 
-            // label4
-            // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(476, 153);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 20);
-            this.label4.TabIndex = 152;
-            this.label4.Text = "Date delai";
             // 
             // ConsultationForm
             // 
